f0add2949540ad6db2d820df990c1fb344df402e
Kamil Patecki <paatorek@gmail.com>
2025-05-17 16:36:44 +0200

    Change root

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/some-tracker/',
   plugins: [svelte()],
   assetsInclude: ['**/*.wasm'],
   build: {
